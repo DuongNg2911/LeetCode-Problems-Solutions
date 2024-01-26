d706d24bb01058934acfdd5fdcf05fd20c68482f
Duong Nguyen (bobdebon) <127082369+DuongNg2911@users.noreply.github.com>
2024-01-26 21:20:21 +0700

    Create find.py

---
+++ medium/MinimumPathSum/find.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        for i in range(1, len(grid[0])):
+            grid[0][i] += grid[0][i-1]
+        
+        for j in range(1, len(grid)):
+            grid[j][0] += grid[j-1][0]
+        
+        for r in range(1, len(grid)):
+            for c in range(1, len(grid[0])):
+                grid[r][c] = min(grid[r-1][c] + grid[r][c], grid[r][c-1]+grid[r][c])
+        
+        return grid[-1][-1]
